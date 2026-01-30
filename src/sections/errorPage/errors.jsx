69cc1de62452a2d0e943ed5654550138787b02c0
KISHORE-SKY <skkishore1273@gmail.com>
2026-01-30 20:25:27 +0530

    error page updated

--- src/sections/errorPage/errors.jsx
+++ src/sections/errorPage/errors.jsx
@@ -1,24 +1,34 @@
 import errorImage from "../../assets/9020.jpg";
 import "./error.css";
 import {Link} from "react-router-dom";
+import FooterSection from '../../sections/footer/contact';
+import Navbar from '../../sections/navbar/navbarSection';
 
 function ErrorElemet() {
     return(
         <>
-            <section className="errorPageContent">
-                <div className="errorPageMessageBox">
-                    <h1>Ooops!</h1>
-                    <div>
-                        <p>Something wrong here...</p>
-                        <p>kindly go back home then click explore tools button.</p>
-                    </div>
-                    <button className="errorPageButton"><Link to="/" className="homeButtonError">Go Back Home</Link></button>
+            <section>
+                <div>
+                    <Navbar />
                 </div>
-                <div className="pictureBoxError">
-                    <img src={errorImage} className="errorPicture"/>
+                <section className="errorPageContent">
+                    <div className="errorPageMessageBox">
+                        <h1>Ooops!</h1>
+                        <div>
+                            <p>Something wrong here...</p>
+                            <p>kindly go back home then explore tools.</p>
+                        </div>
+                        <button className="errorPageButton"><Link to="/" className="homeButtonError">Back to Home</Link></button>
+                        </div>
+                        <div className="pictureBoxError">
+                            <img src={errorImage} className="errorPicture"/>
+                        </div>
+                </section>
+                <div>
+                    <FooterSection />
                 </div>
-                
             </section>
+            
         </>
     );
 }
